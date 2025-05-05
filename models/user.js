a695919733b1b17d4eeaeb50cf681a8169b07b99
Vansh tandel <vanshtandel90@gmail.com>
2025-05-05 23:52:57 +0530

    flash

---
+++ models/user.js
@@ -0,0 +1,16 @@
+const mongoose=require("mongoose")
+const Schema=mongoose.Schema
+const passportLocalMongoose=require("passport-local-mongoose")
+
+// username , hashing salting of password passport passport-local-mongoose j define kari dei ...
+
+const userSchema=new Schema({
+    email:{
+        type:String,
+        required:true
+    },
+})
+
+User.plugin(passportLocalMongoose)
+
+module.exports=mongoose.model("User",User)

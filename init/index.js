1d95a45b4d9c04c7820bcb26f6b30c8348b08f31
Vansh tandel <vanshtandel90@gmail.com>
2025-05-07 04:01:54 +0530

    authorization n authentication

--- init/index.js
+++ init/index.js
@@ -14,6 +14,7 @@ async function main(){
 
 const initDB=async()=>{
     await Listing.deleteMany({})
+    initData.data = initData.data.map((obj)=>({...obj, owner:"681912d2bf4c7bfce6f52e8a"}))
     await Listing.insertMany(initData.data)
     console.log("data was initialized")
 }
